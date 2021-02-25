aec9c036a3abd6a5d0bbc293229e651de964590d
Dorota <d.a.lewinska@gmail.com>
2021-02-25 12:21:37 +0000

    API half way done

--- Handler.cs
+++ Handler.cs
@@ -1,24 +1,55 @@
 using Amazon.Lambda.Core;
+using System.Collections;
+using System.Collections.Generic;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
-       public Response Hello(Request request)
+       public APIGatewayProxyResponse GetCollections()
        {
-           return new Response("Go Serverless v1.0! Your function executed successfully!", request);
+          ArrayList collections = new ArrayList();
+          Collection c1 = new Collection(1, "private", "Mary Brown");
+          Collection c2 = new Collection(2, "private", "John Newman");
+          Collection c3 = new Collection(3, "public", "");
+          collections.Add(c1);
+          collections.Add(c2);
+          collections.Add(c3);
+          string body = JsonSerializer.Serialize(collections);
+          return new APIGatewayProxyResponse {
+            // body must be a string, cannot be ArrayList
+            Body = body,
+            Headers = new Dictionary <string, string>
+            { 
+                { "Content-Type", "application/json" }, 
+                { "Access-Control-Allow-Origin", "*" } 
+            },
+            StatusCode = 200,
+          };
+       }
+       public string GetCollectionById(APIGatewayProxyRequest request)
+       {
+          string paramId = request.PathParameters["paramId"];
+          // to see logger, run in terminal: serverless logs -f get-collection
+          // LambdaLogger.Log("Param id = " +paramId);
+          return paramId;
        }
     }
 
-    public class Response
+    public class Collection
     {
-      public string Message {get; set;}
-      public Request Request {get; set;}
+      public int Id {get;}
+      public string LocationType {get;}
+      public string Name {get;}
 
-      public Response(string message, Request request){
-        Message = message;
-        Request = request;
+      public Collection(int id, string locationType, string name){
+        Id = id;
+        LocationType = locationType;
+        Name = name;
       }
     }
 
