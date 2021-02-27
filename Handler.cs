63d6c021c7836c3d3efffaccbb8d3be28995b27d
Dorota <d.a.lewinska@gmail.com>
2021-02-27 14:02:45 +0000

    Environment variables added

--- Handler.cs
+++ Handler.cs
@@ -4,18 +4,23 @@ using System.Collections.Generic;
 using Amazon.Lambda.APIGatewayEvents;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using MySql.Data.MySqlClient;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace AwsDotnetCsharp
 {
   public class Handler
-  {
+  { 
+    private string DB_HOST = System.Environment.GetEnvironmentVariable("DB_HOST");
+    private string DB_NAME = System.Environment.GetEnvironmentVariable("DB_NAME");
+    private string DB_USER = System.Environment.GetEnvironmentVariable("DB_USER");
+    private string DB_PASSWORD = System.Environment.GetEnvironmentVariable("DB_PASSWORD"); 
     public APIGatewayProxyResponse GetCollections()
     {
       ArrayList collections = new ArrayList();
-      Collection c1 = new Collection(1, "private", "Mary Brown");
-      Collection c2 = new Collection(2, "private", "John Newman");
-      Collection c3 = new Collection(3, "public", "");
+      Collection c1 = new Collection("1", "private", "Mary Brown");
+      Collection c2 = new Collection("2", "private", "John Newman");
+      Collection c3 = new Collection("3", "public", "");
       collections.Add(c1);
       collections.Add(c2);
       collections.Add(c3);
@@ -32,30 +37,49 @@ namespace AwsDotnetCsharp
         StatusCode = 200,
       };
     }
-    public string GetCollectionById(APIGatewayProxyRequest request)
+    public ArrayList GetCollectionById(APIGatewayProxyRequest request)
     {
       string paramId = request.PathParameters["paramId"];
       // to see logger, run in terminal: serverless logs -f get-collection
       // LambdaLogger.Log("Param id = " +paramId);
-      return paramId;
+
+// open up db connection
+      MySqlConnection connection = new MySqlConnection($"server={DB_HOST};user id={DB_USER};password={DB_PASSWORD};port=3306;database={DB_NAME};");
+      connection.Open();
+// prepare mysql query
+      var cmd = connection.CreateCommand();
+      cmd.CommandText = @"SELECT * FROM `collections` WHERE `id` = @id";
+      cmd.Parameters.AddWithValue("@id", paramId);
+// execute the query
+      MySqlDataReader reader = cmd.ExecuteReader();
+      ArrayList collections = new ArrayList();
+// loop through result
+      while (reader.Read())
+      {
+        Collection collection = new Collection(reader.GetString("id"), reader.GetString("locationType"), reader.GetString("name"));
+        collections.Add(collection);
+      }
+// close the connection
+      connection.Close();
+      return collections;
     }
 
     public string CreateCollection(APIGatewayProxyRequest request)
     {
       string body = request.Body;
       string paramId = request.PathParameters["paramId"];
-      string msg = "Post request with param = " +paramId;
+      string msg = "Post request with param = " + paramId;
       return body;
     }
   }
 
   public class Collection
   {
-    public int Id { get; }
-    public string LocationType { get; }
-    public string Name { get; }
+    public string Id { get; set; }
+    public string LocationType { get; set; }
+    public string Name { get; set; }
 
-    public Collection(int id, string locationType, string name)
+    public Collection(string id, string locationType, string name)
     {
       Id = id;
       LocationType = locationType;
